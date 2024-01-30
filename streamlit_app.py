88ffe342bb6e7e108590258e2d0ae5f6eb57c40a
patchanarin2024 <158025941+patchanarin2024@users.noreply.github.com>
2024-01-30 13:51:12 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,66 @@
-import altair as alt
-import numpy as np
-import pandas as pd
 import streamlit as st
+import pandas as pd
+import plotly.express as px
+import re
 
+st.set_page_config(page_title=" CEHCK CARD", page_icon=":bar_chart:", layout="wide")
+
+
+def convert_google_sheet_url(url):
+    # Regular expression to match and capture the necessary part of the URL
+    pattern = r'https://docs\.google\.com/spreadsheets/d/([a-zA-Z0-9-_]+)(/edit#gid=(\d+)|/edit.*)?'
+
+    # Replace function to construct the new URL for CSV export
+    # If gid is present in the URL, it includes it in the export URL, otherwise, it's omitted
+    replacement = lambda m: f'https://docs.google.com/spreadsheets/d/{m.group(1)}/export?' + (f'gid={m.group(3)}&' if m.group(3) else '') + 'format=csv'
+
+    # Replace using regex
+    new_url = re.sub(pattern, replacement, url)
+
+    return new_url
+
+
+# Replace with your modified URL
+url = 'https://docs.google.com/spreadsheets/d/1ED8Mj5bY629NlTNvAOQXoVkSDHVKYnLwyTpzXaSLJ60/edit#gid=0'
+
+new_url = convert_google_sheet_url(url)
+
+
+df = pd.read_csv(new_url)
+
+
+st.sidebar.header("Please Filter Here:")
+tent = st.sidebar.multiselect(
+    "Select the Tent:",
+    options=df["Tent"].unique()
+    
+)
+
+df_selection = df.query(
+    "Tent == @tent"
+)
+
+total  = int(df_selection["card_code"].sum())
+
+st.title(f"Total   {total:,}")
+
+
+custom_css = """
+    .centered-header th {
+        text-align: center;
+    }
+    
+    .centered-text td {
+        text-align: center;
+    }
 """
-# Welcome to Streamlit!
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+st.write(f"<style>{custom_css}</style>", unsafe_allow_html=True)
+st.write(df_selection.to_html(classes=["centered-header", "centered-text"],index=False, escape=False), unsafe_allow_html=True)
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
 
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
+ 
 
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
+st.markdown("""---""")
 
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
-
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
